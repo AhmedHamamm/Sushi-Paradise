d96c6292f8f40bb41452f2bf19bb876461c5680c
Ahmed Hamamm <ahamam2000@yahoo.com>
2024-07-12 23:56:09 +0300

    Add dark theme, scroll reveal animation, scroll-up function, and make the site responsive
    - Added a dark theme toggle in JavaScript and CSS, enabling users to switch between light and dark modes for better accessibility and user preference.
    - Implemented scroll reveal animations using JavaScript and CSS to animate elements as they enter the viewport, enhancing user engagement.
    - Developed a scroll-up button in JavaScript that appears when the user scrolls down, allowing easy navigation back to the top.
    - Applied responsive design techniques across the entire site.

--- assets/js/main.js
+++ assets/js/main.js
@@ -37,3 +37,88 @@ const scrollHeader = () => {
 };
 
 window.addEventListener("scroll", scrollHeader);
+
+// Show Scroll Up
+const scrollUp = () => {
+  const scrollUp = document.getElementById("scroll-up");
+  // When the scroll is higher than 350 viewport height. add the show scroll class to the a tag with scrollup id
+  this.scrollY >= 350
+    ? scrollUp.classList.add("show-scroll")
+    : scrollUp.classList.remove("show-scroll");
+};
+window.addEventListener("scroll", scrollUp);
+
+// scroll section active link
+const section = document.querySelectorAll("section[id]");
+
+const scrollActive = () => {
+  const scrollY = window.scrollY;
+  section.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute("id");
+    sectionClass = document.querySelector(
+      ".nav-menu a[href*=" + sectionId + "]"
+    );
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      sectionClass.classList.add("active-link");
+    } else {
+      sectionClass.classList.remove("active-link");
+    }
+  });
+};
+window.addEventListener("scroll", scrollActive);
+
+// Dark Light theme
+const themeBtn = document.getElementById("theme-button");
+const darkTheme = "dark-theme";
+const iconTheme = "ri-sun-line";
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem("selected-theme");
+const selectedIcon = localStorage.getItem("selected-icon");
+
+// I obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () =>
+  document.body.classList.contains(darkTheme) ? "dark" : "light";
+const getCurrentIcon = () =>
+  themeBtn.classList.contains(iconTheme) ? "ri-moon-line" : "ri-sun-line";
+
+// I validate if the user previously chose a topic
+if (selectedTheme) {
+  // If the validation is fulfilled, I recover which theme was selected
+  document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
+    darkTheme
+  );
+  themeBtn.classList[selectedIcon === "ri-moon-line" ? "add" : "remove"](
+    iconTheme
+  );
+}
+
+// Activate / deactivate the theme manually with the button
+themeBtn.addEventListener("click", () => {
+  // Add or remove the dark / icon theme
+  document.body.classList.toggle(darkTheme);
+  themeBtn.classList.toggle(iconTheme);
+  // I save the theme and the current icon that the user choose
+  localStorage.setItem("selected-theme", getCurrentTheme());
+  localStorage.setItem("selected-icon", getCurrentIcon());
+});
+
+// Scrollreveal
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "60px",
+  duration: 2500,
+  delay: 400,
+  // reset: true,
+});
+
+sr.reveal(
+  `.home-img, .newsletter-container, .footer-logo, 
+  .footer-description, .footer-content, .footer-info`
+);
+sr.reveal(`.home-data`, { origin: "bottom" });
+sr.reveal(`.about-data, .recently-data`, { origin: "left" });
+sr.reveal(`.about-img, .recently-img`, { origin: "right" });
+sr.reveal(`.popular-card`, { interval: 100 });
